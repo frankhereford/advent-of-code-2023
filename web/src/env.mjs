7a73ee01d5c1f3ed7e45eebfd7cfc6268be65e79
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-10 09:25:01 -0500

    openai key

--- web/src/env.mjs
+++ web/src/env.mjs
@@ -8,7 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-    OPENAI_KEY: z.string().min(1),
+    OPENAI_API_KEY: z.string().min(1),
   },
 
   /**
@@ -26,7 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    OPENAI_KEY: process.env.OPENAI_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
