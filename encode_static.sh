7a4a3d909fcd93172f7c868195171a71c70b738d
Frank Hereford <github@frankhereford.com>
2023-10-17 23:02:18 +0000

    use new helper tooling

--- encode_static.sh
+++ encode_static.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-docker compose run download CKsLgdbzlfk
-#docker compose run download OUoA-dKQhtc
-docker compose run download S7askw5Z084
-#docker compose run download yCPtemRqJks
-docker compose run download bVy32Lx41-o
-#docker compose run download ORhPhhcdtkA
-docker compose run download N0OPOsN3kA8
-docker compose run download htdAiUyoFDU
-docker compose run download nqtwZXp88w0
-docker compose run download vcZv6yfBcLk
-docker compose run download mCcNtjVBL3w
-docker compose run download IeRJnQBySCg
+./add_video.sh CKsLgdbzlfk
+#./add_video.sh OUoA-dKQhtc
+./add_video.sh S7askw5Z084
+#./add_video.sh yCPtemRqJks
+./add_video.sh bVy32Lx41-o
+#./add_video.sh ORhPhhcdtkA
+./add_video.sh N0OPOsN3kA8
+./add_video.sh htdAiUyoFDU
+./add_video.sh nqtwZXp88w0
+./add_video.sh vcZv6yfBcLk
+./add_video.sh mCcNtjVBL3w
+./add_video.sh IeRJnQBySCg
