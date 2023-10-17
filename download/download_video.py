58082f166f8ac16e029cbe95be63a1272b0d0986
Frank Hereford <github@frankhereford.com>
2023-10-17 21:35:32 +0000

    daemonize this and organize the workdir

--- download/download_video.py
+++ download/download_video.py
@@ -8,16 +8,17 @@ from vtt_to_srt.vtt_to_srt import ConvertFile
 
 
 def append_to_redis_list(redis_host='redis', redis_port=6379, key_to_append='encode_queue', value_to_append=''):
-    # Initialize the Redis client
     r = redis.Redis(host=redis_host, port=redis_port)
-
-    # Append the value to the list
     r.rpush(key_to_append, value_to_append)
     print(f"Appended '{value_to_append}' to the list stored at key {key_to_append}")
 
 def download_youtube_video(video_id):
     try:
-        subprocess.run(['timeout', '30s', 'yt-dlp', '--write-subs', '--write-auto-sub', '-o', '%(id)s.mp4', '-f', "best[height<=?360]", video_id])
+        # Create a directory for the video
+        video_dir = f"/application/workdir/downloads/{video_id}"
+        os.makedirs(video_dir, exist_ok=True)
+
+        subprocess.run(['timeout', '60s', 'yt-dlp', '--write-subs', '--write-auto-sub', '-o', f"{video_dir}/%(id)s.mp4", '-f', "best[height<=?360]", video_id])
     except Exception as e:
         print(f"Something went wrong: {e}")
 
@@ -28,11 +29,16 @@ def convert_subtitles(video_id):
     except Exception as e:
         print(f"Couldn't convert subttitles: {e}")
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        video_id = sys.argv[1]
+def poll_redis_list(redis_host='redis', redis_port=6379, queue_to_poll='start_queue'):
+    r = redis.Redis(host=redis_host, port=redis_port)
+    while True:
+        _, video_id = r.blpop(queue_to_poll)
+        video_id = video_id.decode('utf-8')
+        print(f"Got video ID {video_id} from {queue_to_poll}")
         download_youtube_video(video_id)
         convert_subtitles(video_id)
         append_to_redis_list(value_to_append=video_id)
-    else:
-        print("Please provide a YouTube video ID as an argument.")
+
+
+if __name__ == "__main__":
+    poll_redis_list()
