769280112d7589163233b54f977a8fc4ffc109e5
Frank Hereford <github@frankhereford.com>
2023-10-17 05:48:26 +0000

    never enough good static

--- encode_static.sh
+++ encode_static.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose run download CKsLgdbzlfk
-docker compose run download OUoA-dKQhtc
+#docker compose run download OUoA-dKQhtc
 docker compose run download S7askw5Z084
 #docker compose run download yCPtemRqJks
 docker compose run download bVy32Lx41-o
