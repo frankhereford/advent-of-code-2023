6a3a5bb8429d252683bee43184873c2e1bac926b
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-20 16:41:30 -0500

    make this easier to work with

--- nextjs/src/utils/videos_of_static.ts
+++ nextjs/src/utils/videos_of_static.ts
@@ -1,7 +1,12 @@
 // myStrings.ts
-const videos_of_static: string[] = [
-  "CKsLgdbzlfk", "S7askw5Z084", "bVy32Lx41-o",
-  "N0OPOsN3kA8", "htdAiUyoFDU", "nqtwZXp88w0",
-  "vcZv6yfBcLk", "mCcNtjVBL3w", "IeRJnQBySCg"
-];
+const videos_of_static: string[] = []
+  videos_of_static.push("CKsLgdbzlfk")
+  videos_of_static.push("S7askw5Z084")
+  videos_of_static.push("bVy32Lx41-o")
+  videos_of_static.push("N0OPOsN3kA8")
+  videos_of_static.push("htdAiUyoFDU")
+  videos_of_static.push("nqtwZXp88w0")
+  videos_of_static.push("vcZv6yfBcLk")
+  videos_of_static.push("mCcNtjVBL3w")
+  videos_of_static.push("IeRJnQBySCg")
 export default videos_of_static;
