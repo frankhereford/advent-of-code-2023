cd321b1663e099fd061caa5210eb363bc0c5c657
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-20 21:50:03 -0500

    this is helpful

---
+++ encode_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./add_video.sh riO5p_Qi8Vc 
+./add_video.sh asgpQP81ZqU
+./add_video.sh KiQuCeJqQI0
+./add_video.sh ir1YXtdhRPg
+./add_video.sh Iiny1GrfhYM
+./add_video.sh e5AGHEUxLME
+./add_video.sh CkZyZFa5qO0
+
