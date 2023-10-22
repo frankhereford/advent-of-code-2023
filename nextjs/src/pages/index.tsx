1ff97b7208ab84e9f877af21d0acd869916e4833
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-22 11:20:01 -0500

    less application in here

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -2,18 +2,22 @@ import { useEffect, useState } from 'react';
 import Head from "next/head";
 import { api } from "~/utils/api";
 
-import Polaroid from './components/Polaroid';
+import Polaroid from '~/pages/components/Polaroid';
 
 export default function Home() {
-  const [videoIDs, setVideoIDs] = useState<(string | null)[]>([null, null]);
+  //const [videoIDs, setVideoIDs] = useState<(string | null)[]>([null, null]);
+  const [videoIDs, setVideoIDs] = useState<(string | null)[]>(Array.from({ length: 2 }, () => null));
   const [label, setLabel] = useState<(string | null)>('tuna & fish');
 
-  const videos = api.media.get_random_videos.useQuery({ length: 2 })
+  //const videos = api.media.get_random_videos.useQuery({ length: 2 }, {
+    //enabled: true,
+    //refetchOnWindowFocus: false,
+  //})
 
-  useEffect(() => {
-    if (!videos.data) return;
-    setVideoIDs(videos.data);
-  }, [videos.data]);
+  //useEffect(() => {
+    //if (!videos.data) return;
+    //setVideoIDs(videos.data);
+  //}, [videos.data]);
 
   return (
     <>
