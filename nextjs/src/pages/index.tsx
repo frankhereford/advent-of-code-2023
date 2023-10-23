5a41d15ce551b1755325d4a3c6c82ce50429425c
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-23 17:07:11 -0500

    strip out homepage

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,41 +1,13 @@
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Head from "next/head";
 import { api } from "~/utils/api";
 
 import Polaroid from '~/pages/components/Polaroid';
 
 export default function Home() {
-  const [videoIDs, setVideoIDs] = useState<(string | null)[]>(Array.from({ length: 2 }, () => null));
+  const [videoIDs, setVideoIDs] = useState<(string | undefined)[]>(Array.from({ length: 2 }, () => undefined));
   const [label, setLabel] = useState<(string | undefined)>('');
   const [topic, setTopic] = useState<(string | undefined)>('');
-  const [searchYouTube, setSearchYoutube] = useState<(boolean)>(false);
-
-  const topicQuery = api.openai.get_topic.useQuery({ hint: '' }, {
-    trpc: { context: { skipBatch: true, }, },
-  });
-
-  const videoQuery = api.youtube.get_video.useQuery({ topic: topic! }, {
-    enabled: searchYouTube,
-    //enabled: false,
-    trpc: { context: { skipBatch: true, }, },
-  });
-
-  useEffect(() => {
-    if (topicQuery.data?.label === '') return;
-    if (topicQuery.data) {
-      setLabel(topicQuery.data.label)
-      setTopic(topicQuery.data.topic)
-      setSearchYoutube(true)
-    }
-  }, [topicQuery.data]);
-
-  useEffect(() => {
-    if (!videoQuery.data) return;
-    if (videoQuery.data) {
-      console.log(videoQuery.data.videos)
-      setVideoIDs(videoQuery.data.videos)
-    }
-  }, [videoQuery.data]);
 
   return (
     <>
