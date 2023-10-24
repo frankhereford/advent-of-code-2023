2282d29d8e70764487ca03cbde5b48c4fb2a1544
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-23 23:34:27 -0500

    add a bunch of news

--- encode_tests.sh
+++ encode_tests.sh
@@ -8,3 +8,18 @@
 ./add_video.sh e5AGHEUxLME
 ./add_video.sh CkZyZFa5qO0
 
+./add_video.sh Lmd6CHah7Wg
+./add_video.sh hUzlpIxXbtM
+./add_video.sh 3WmNP6QBSQs
+./add_video.sh Kj_1KG7mbJE
+./add_video.sh BGTFn25clr4
+./add_video.sh 1PvclFMBXmw
+./add_video.sh oeWjVLwV3Zk
+./add_video.sh ZEOGJJ7UKFM
+./add_video.sh PGom7kU7b7A
+./add_video.sh zQHptQcYsGk
+./add_video.sh PvQAbl6G7uY
+./add_video.sh Ev6IJOlROto
+./add_video.sh eGI7VymjSho
+./add_video.sh FHJs3TwgsUQ
+./add_video.sh gbsTaGYgdfI
