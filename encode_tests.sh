0db382ff26ab693b72b81fd8e52fb4e4397a678f
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-23 23:38:15 -0500

    just use the news

--- encode_tests.sh
+++ encode_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # random frank stuff
-./add_video.sh riO5p_Qi8Vc 
-./add_video.sh asgpQP81ZqU
-./add_video.sh KiQuCeJqQI0
-./add_video.sh ir1YXtdhRPg
-./add_video.sh Iiny1GrfhYM
-./add_video.sh e5AGHEUxLME
-./add_video.sh CkZyZFa5qO0
+#./add_video.sh riO5p_Qi8Vc 
+#./add_video.sh asgpQP81ZqU
+#./add_video.sh KiQuCeJqQI0
+#./add_video.sh ir1YXtdhRPg
+#./add_video.sh Iiny1GrfhYM
+#./add_video.sh e5AGHEUxLME
+#./add_video.sh CkZyZFa5qO0
 
 # news articles
 ./add_video.sh Lmd6CHah7Wg
