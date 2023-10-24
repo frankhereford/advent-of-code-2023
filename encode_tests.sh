55bbd9e21fa1126bba99eed3aad91d5acf2bacf2
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-24 10:50:47 -0500

    murder hornets

--- encode_tests.sh
+++ encode_tests.sh
@@ -9,29 +9,32 @@
 #./add_video.sh e5AGHEUxLME
 #./add_video.sh CkZyZFa5qO0
 
+# murder hornets
+./add_video.sh -gAVlh-7WZM
+
 # news articles
-./add_video.sh Lmd6CHah7Wg
-./add_video.sh hUzlpIxXbtM
-./add_video.sh 3WmNP6QBSQs
-./add_video.sh Kj_1KG7mbJE
-./add_video.sh BGTFn25clr4
-./add_video.sh 1PvclFMBXmw
-./add_video.sh oeWjVLwV3Zk
-./add_video.sh ZEOGJJ7UKFM
-./add_video.sh PGom7kU7b7A
-./add_video.sh zQHptQcYsGk
-./add_video.sh PvQAbl6G7uY
-./add_video.sh Ev6IJOlROto
-./add_video.sh eGI7VymjSho
-./add_video.sh FHJs3TwgsUQ
-./add_video.sh gbsTaGYgdfI
-./add_video.sh JeWpt0BEP7E
-./add_video.sh oMNDDoSaaQs
-./add_video.sh XmeeEpWxfRY
-./add_video.sh w_uOSImPSi8
-./add_video.sh cOuRwMW-Mrc
-./add_video.sh 0_8YEglCFdU
-./add_video.sh anfjhvv_dbM
-./add_video.sh wcAbBMBjxqo
-./add_video.sh bSeCpFCS32M
-./add_video.sh o5KIiDcs8no
+#./add_video.sh Lmd6CHah7Wg
+#./add_video.sh hUzlpIxXbtM
+#./add_video.sh 3WmNP6QBSQs
+#./add_video.sh Kj_1KG7mbJE
+#./add_video.sh BGTFn25clr4
+#./add_video.sh 1PvclFMBXmw
+#./add_video.sh oeWjVLwV3Zk
+#./add_video.sh ZEOGJJ7UKFM
+#./add_video.sh PGom7kU7b7A
+#./add_video.sh zQHptQcYsGk
+#./add_video.sh PvQAbl6G7uY
+#./add_video.sh Ev6IJOlROto
+#./add_video.sh eGI7VymjSho
+#./add_video.sh FHJs3TwgsUQ
+#./add_video.sh gbsTaGYgdfI
+#./add_video.sh JeWpt0BEP7E
+#./add_video.sh oMNDDoSaaQs
+#./add_video.sh XmeeEpWxfRY
+#./add_video.sh w_uOSImPSi8
+#./add_video.sh cOuRwMW-Mrc
+#./add_video.sh 0_8YEglCFdU
+#./add_video.sh anfjhvv_dbM
+#./add_video.sh wcAbBMBjxqo
+#./add_video.sh bSeCpFCS32M
+#./add_video.sh o5KIiDcs8no
