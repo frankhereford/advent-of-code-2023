80284ec1206b26275ae0b792f2817d21baf11b6c
Frank Hereford <frank@frankhereford.com>
2023-12-02 16:39:15 +0000

    Comment out console.log statement in postMessageToWorker function

--- nextjs/src/pages/webworkers/workerHelpers.js
+++ nextjs/src/pages/webworkers/workerHelpers.js
@@ -1,5 +1,5 @@
 export function postMessageToWorker(do_print, message) {
-  console.log("do_print", do_print, "message", message)
+  //console.log("do_print", do_print, "message", message)
   if (do_print) {
     self.postMessage({ action: 'statusUpdate', message });
   }
