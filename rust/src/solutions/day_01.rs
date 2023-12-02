d3375adf97b492e1b1547d063a1bac146fcaefed
Frank Hereford <frank@frankhereford.com>
2023-12-02 16:39:33 +0000

    Fix input file path in day_01.rs

--- rust/src/solutions/day_01.rs
+++ rust/src/solutions/day_01.rs
@@ -10,7 +10,8 @@ extern "C" {
 pub fn solution_part_1(_n: u32) -> () {
     postMessageToWorker(true, "Concatenate the first and last digits found in a string.\n");
     let mut iteration = -1;
-    let content = include_str!("input/day_01_test_input.txt");
+    // let content = include_str!("input/day_01_test_input.txt");
+    let content = include_str!("input/day_01_input.txt");
 
     let is_digit_regex  = Regex::new(r"\d").unwrap();
     let mut codes: Vec<u32> = Vec::new();
@@ -19,10 +20,9 @@ pub fn solution_part_1(_n: u32) -> () {
         // Provide a mechanism to limit the volume of output on the console.
         iteration += 1;
         let mut show_message = false;
-        if (iteration) % 50 == 0  {
+        if (iteration) % 100 == 0  {
             show_message = true;
         }
-
 
         let characters: Vec<_> = line.chars().collect();
         if characters[0] == '#' {
@@ -32,14 +32,13 @@ pub fn solution_part_1(_n: u32) -> () {
 
         postMessageToWorker(show_message, " ");
         postMessageToWorker(show_message, &format!("line: {}", line));
-        // postMessageToWorker(show_message, &format!("characters: {:?}", characters));
 
         let mut first_digit: Option<&char> = None;
         let mut last_digit: Option<&char> = None;
 
         for (_index, character) in characters.iter().enumerate() {
             if is_digit_regex.is_match(character.to_string().as_str()) {
-                // postMessageToWorker(show_message, &format!("index: {}, character: {}", index, character));
+                postMessageToWorker(show_message, &format!("Found a digit: {}", character));
                 if first_digit == None {
                     first_digit = Some(character);
                 }
@@ -47,13 +46,10 @@ pub fn solution_part_1(_n: u32) -> () {
             }
         }
 
-        // postMessageToWorker(show_message, &format!("first_digit: {:?}, last_digit: {:?}", first_digit, last_digit));
-
         let found_code_string = format!("{}{}", first_digit.unwrap(), last_digit.unwrap());
         let found_code_int = found_code_string.parse::<u32>().unwrap();
         postMessageToWorker(show_message, &format!("found_code: {}", found_code_int));
         codes.push(found_code_int);
-
     });
 
     let sum: u32 = codes.iter().sum();
