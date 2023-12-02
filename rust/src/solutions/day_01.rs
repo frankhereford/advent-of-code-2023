0254ac2fbf3c6f4c7b8c1186441ba48aae0c9977
Frank Hereford <frank@frankhereford.com>
2023-12-02 16:14:50 +0000

    Refactor code to extract and store found codes

--- rust/src/solutions/day_01.rs
+++ rust/src/solutions/day_01.rs
@@ -13,23 +13,25 @@ pub fn solution(_n: u32) -> () {
     let content = include_str!("input/day_01_test_input.txt");
     let is_digit_regex  = Regex::new(r"\d").unwrap();
 
+    let mut codes: Vec<u32> = Vec::new();
+
     content.lines().for_each(|line| {
         let characters: Vec<_> = line.chars().collect();
         if characters[0] == '#' {
-            //postMessageToWorker("Skipping line because it is a comment.");
+            // postMessageToWorker("Skipping line because it is a comment.");
             return;
         }
 
         postMessageToWorker(" ");
         postMessageToWorker(&format!("line: {}", line));
-        postMessageToWorker(&format!("characters: {:?}", characters));
+        // postMessageToWorker(&format!("characters: {:?}", characters));
 
         let mut first_digit: Option<&char> = None;
         let mut last_digit: Option<&char> = None;
 
-        for (index, character) in characters.iter().enumerate() {
+        for (_index, character) in characters.iter().enumerate() {
             if is_digit_regex.is_match(character.to_string().as_str()) {
-                postMessageToWorker(&format!("index: {}, character: {}", index, character));
+                // postMessageToWorker(&format!("index: {}, character: {}", index, character));
                 if first_digit == None {
                     first_digit = Some(character);
                 }
@@ -37,10 +39,12 @@ pub fn solution(_n: u32) -> () {
             }
         }
 
-        postMessageToWorker(&format!("first_digit: {:?}, last_digit: {:?}", first_digit, last_digit));
+        // postMessageToWorker(&format!("first_digit: {:?}, last_digit: {:?}", first_digit, last_digit));
+
         let found_code_string = format!("{}{}", first_digit.unwrap(), last_digit.unwrap());
         let found_code_int = found_code_string.parse::<u32>().unwrap();
         postMessageToWorker(&format!("found_code: {}", found_code_int));
+        codes.push(found_code_int);
 
     });
 }
