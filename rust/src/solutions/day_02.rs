bfa24a617da76c2168b367ba4277e5b48dc7cab0
Frank Hereford <frank@frankhereford.com>
2023-12-02 20:10:56 +0000

    Add regex functionality to extract game and demonstrations from input string

--- rust/src/solutions/day_02.rs
+++ rust/src/solutions/day_02.rs
@@ -1,5 +1,6 @@
 //use web_sys::console;
 use wasm_bindgen::prelude::*;
+use regex::Regex;
 
 #[wasm_bindgen(module = "/src/solutions/workerHelpers.js")]
 extern "C" {
@@ -7,10 +8,13 @@ extern "C" {
 }
 
 pub fn solution_part_1() -> () {
-    postMessageToWorker(true, "Part 1: \n");
+    postMessageToWorker(true, "Part 1: Check game validity by color counts.\n");
     let mut iteration = -1;
     let content = include_str!("input/day_02_part_1_test_input.txt");
     //let content = include_str!("input/day_02_input.txt");
+
+    let input_chunks = Regex::new(r"Game (\d+): (.*)").unwrap();
+    
 
     content.lines().for_each(|line| {
         // Provide a mechanism to limit the volume of output on the console.
@@ -20,6 +24,7 @@ pub fn solution_part_1() -> () {
             show_message = true;
         }
 
+
         let characters: Vec<_> = line.chars().collect();
         if characters[0] == '#' {
             // postMessageToWorker(show_message, "Skipping line because it is a comment.");
@@ -28,6 +33,25 @@ pub fn solution_part_1() -> () {
 
         postMessageToWorker(show_message, " ");
         postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
+
+
+        // how use regex to extract substrings from an input string
+        if let Some(caps) = input_chunks.captures(line) {
+            let game = caps.get(1).map_or("", |m| m.as_str());
+            let demonstrations_as_string = caps.get(2).map_or("", |m| m.as_str());
+            postMessageToWorker(show_message, &format!("demonstrations_as_string: {}", demonstrations_as_string));
+            let demonstrations: Vec<&str> = demonstrations_as_string.split(';').collect();
+
+            for demonstration in demonstrations {
+                postMessageToWorker(show_message, &format!("demonstration: {}", demonstration));
+            }
+
+            //postMessageToWorker(show_message, &format!("game: {}, parameters: {}, proposed_game: {}", game, parameters, proposed_game));
+            postMessageToWorker(show_message, &format!("game: {}", game));
+
+        }
+
+
     });
 }
 
