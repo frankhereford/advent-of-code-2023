56dc64058322be9a87c424c1cfab0b37f762b95f
Frank Hereford <frank@frankhereford.com>
2023-12-02 15:27:18 +0000

    ok building cleanly

--- rust/src/solutions/day_01.rs
+++ rust/src/solutions/day_01.rs
@@ -23,33 +23,13 @@ pub fn solution(_n: u32) -> () {
         postMessageToWorker(&format!("line: {}", line));
         postMessageToWorker(&format!("characters: {:?}", characters));
 
-        let mut first_digit = ' ';
-        let mut first_digit_location: i32 = -1;
+        //let mut first_digit = ' ';
+        //let mut first_digit_location: i32 = -1;
 
-        let mut last_digit = ' ';
-        let mut last_digit_location: i32 = -1;
+        //let mut last_digit = ' ';
+        //let mut last_digit_location: i32 = -1;
 
 
-        /*
-        for (index, character) in characters.iter().enumerate() {
-            let re = Regex::new(r"\d").unwrap();
-            let is_match = re.is_match(character.to_string().as_str());
-            if is_match {
-                if first_digit_location < 0 {
-                    postMessageToWorker(&format!("Found first digit: {}", character));
-                    first_digit_location = index as i32;
-                }
-                last_digit_location = index;
-            }   
-
-            let first_digit = characters[first_digit_location as usize];
-            let last_digit = characters[last_digit_location as usize];
-
-        }
-        */
-
-        //postMessageToWorker(&format!("First digit, {}, found at location: {}", first_digit, first_digit_location));
-        //postMessageToWorker(&format!("Last digit, {}, found at location: {}", last_digit, last_digit_location));
     });
 
 }
