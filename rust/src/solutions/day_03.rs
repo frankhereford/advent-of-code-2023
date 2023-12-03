bdd2c307581c29570e9b3311c4a128bf60775dde
Frank Hereford <frank@frankhereford.com>
2023-12-03 16:22:33 +0000

    feat(day_03): enhance logging and control flow for solution_part_1
    This commit makes several updates to the day_03.rs file:
    
    - The `#![allow(unreachable_code)]` directive has been added, likely to suppress compiler warnings about code paths that may not be executed.
    - Improved clarity in the logging message within `solution_part_1` by specifying what is being calculated: "Summation of symbol adjacent scalars."
    - Modified the logging frequency from every 300 iterations to every single iteration for increased verbosity during the problem-solving process.
    - Changed the conditional check character from '#' to '`' for skipping lines, indicating a shift in what is considered a comment within the input data.
    - Added additional logging messages to print out the current iteration and the input line being processed.
    - Commented out the section of code where digits were being checked with regex, removing unnecessary matches on digits without deleting the logic for potential future use.
    - In `solution_part_2`, a premature return was added at the beginning of the function, which halts its execution path. This indicates either a work-in-progress or a temporary way to skip executing part 2's code.
    
    These changes indicate that the developer is refining the debugging output for better visibility during development or solving the given problem more effectively. The addition of an early return in `solution_part_2` needs further clarification as to whether it's intended or something to be implemented later.

--- rust/src/solutions/day_03.rs
+++ rust/src/solutions/day_03.rs
@@ -1,3 +1,4 @@
+#![allow(unreachable_code)]
 //use web_sys::console;
 use wasm_bindgen::prelude::*;
 use regex::Regex;
@@ -8,7 +9,7 @@ extern "C" {
 }
 
 pub fn solution_part_1() -> () {
-    postMessageToWorker(true, "Part 1: \n");
+    postMessageToWorker(true, "Part 1: Sumation of symbol adjacent scalars.\n");
     let mut iteration = -1;
     let content = include_str!("input/day_03_part_1_test_input.txt");
     // let content = include_str!("input/day_XX_input.txt");
@@ -19,30 +20,33 @@ pub fn solution_part_1() -> () {
         // Provide a mechanism to limit the volume of output on the console.
         iteration += 1;
         let mut show_message = false;
-        if (iteration) % 300 == 0  {
+        if (iteration) % 1 == 0  {
             show_message = true;
         }
 
         let characters: Vec<_> = line.chars().collect();
-        if characters[0] == '#' {
+        if characters[0] == '`' {
             // postMessageToWorker(show_message, "Skipping line because it is a comment.");
             return;
         }
 
+        postMessageToWorker(show_message, " ");
+        postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
+
         for character in characters {
             postMessageToWorker(show_message, &format!("Character: {}", character));
-            if is_digit_regex.is_match(character.to_string().as_str()) {
-            }
+            // if is_digit_regex.is_match(character.to_string().as_str()) {
+                // postMessageToWorker(show_message, &format!("Found a digit: {}", character));
+            // }
         }
 
 
-        postMessageToWorker(show_message, " ");
-        postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
     });
 }
 
 
 pub fn solution_part_2() -> () {
+    return;
     postMessageToWorker(true, "Part 2: \n");
     let mut iteration = -1;
     let content = include_str!("input/day_03_part_1_test_input.txt");
