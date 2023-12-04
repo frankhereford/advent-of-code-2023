56586bf88cb88fa755e27529b450e6c89f1bc981
Frank Hereford <frank@frankhereford.com>
2023-12-04 23:46:24 +0000

    this is so helpful
    Enable status message logging in workerHelpers
    
    The postMessageToWorker function now logs the message to the console when do_print is true. This allows for easier debugging of worker status messages without cluttering the console output when do_print is false.

--- nextjs/src/pages/webworkers/workerHelpers.js
+++ nextjs/src/pages/webworkers/workerHelpers.js
@@ -1,6 +1,7 @@
 export function postMessageToWorker(do_print, message) {
   //console.log("do_print", do_print, "message", message)
   if (do_print) {
+    console.log(message)
     self.postMessage({ action: 'statusUpdate', message });
   }
 }
