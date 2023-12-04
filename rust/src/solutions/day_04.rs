54b3b2b867c68008f84eec5157f7f221712bae30
Frank Hereford <frank@frankhereford.com>
2023-12-04 22:33:22 +0000

    Improve regex parsing and add digit extraction
    Refactored the day_04.rs module to include regex for input chunking and implemented a function to split and parse extracted number strings into vectors of u32. This change will allow more precise data manipulation for input processing in the solution logic.

--- rust/src/solutions/day_04.rs
+++ rust/src/solutions/day_04.rs
@@ -1,7 +1,8 @@
 #![allow(unreachable_code)]
 #![allow(unused_variables)]
-//use web_sys::console;
+// use web_sys::console;
 use wasm_bindgen::prelude::*;
+use regex::Regex;
 
 #[wasm_bindgen(module = "/src/solutions/workerHelpers.js")]
 extern "C" {
@@ -13,6 +14,8 @@ pub fn solution_part_1() -> () {
     let mut iteration = -1;
     let content = include_str!("input/day_04_part_1_test_input.txt");
     // let content = include_str!("input/day_04_input.txt");
+
+    let input_chunks = Regex::new(r"Card (\d+): (.*) \| (.*)").unwrap();
 
     content.lines().for_each(|line| {
         // Provide a mechanism to limit the volume of output on the console.
@@ -30,9 +33,33 @@ pub fn solution_part_1() -> () {
 
         postMessageToWorker(show_message, " ");
         postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
+
+
+        if let Some(caps) = input_chunks.captures(line) {
+            let card = caps.get(1).map_or("", |m| m.as_str());
+            postMessageToWorker(show_message, &format!("card: {}", card));
+            let card_int = card.parse::<u32>().expect("Should be able to parse game");
+            let winning_numbers_as_string = caps.get(2).map_or("", |m| m.as_str());
+            let our_numbers_as_string = caps.get(3).map_or("", |m| m.as_str());
+            let winning_numbers = split_digits_over_whitespace(winning_numbers_as_string);
+            let our_numbers = split_digits_over_whitespace(our_numbers_as_string);
+            postMessageToWorker(show_message, &format!("winning numbers: {:?}", winning_numbers));
+            postMessageToWorker(show_message, &format!("our numbers: {:?}", our_numbers));
+        }
+
     });
 }
 
+fn split_digits_over_whitespace(input: &str) -> Vec<u32> {
+    let whitespace = Regex::new(r"\s+").unwrap();
+    let parts_as_strings: Vec<&str> = whitespace.split(input).collect();
+    let mut found_numbers: Vec<u32> = Vec::new();
+    for number in parts_as_strings {
+        let number_as_int = number.parse::<u32>().expect("Should be able to parse number");
+        found_numbers.push(number_as_int);
+    }
+    found_numbers
+}
 
 pub fn solution_part_2() -> () {
     return;
