88ebfaecee3b211cce073d259910eddbc51496d7
Frank Hereford <frank@frankhereford.com>
2023-12-04 22:54:31 +0000

    Enhance day_04 solution with better output & scoring logic
    Updated the regular expression in `day_04.rs` to handle multiple spaces and implemented a scoring system for cards. Now using the `console` from `web_sys` to log output, which was previously commented out. Introduced a vector to store card scores and calculate a total score, improving result traceability and debugging by adding more verbose logging throughout the process. This may affect performance due to increased console output and should be profiled if used in a production environment.

--- rust/src/solutions/day_04.rs
+++ rust/src/solutions/day_04.rs
@@ -1,6 +1,6 @@
 #![allow(unreachable_code)]
 #![allow(unused_variables)]
-// use web_sys::console;
+use web_sys::console;
 use wasm_bindgen::prelude::*;
 use regex::Regex;
 
@@ -15,8 +15,9 @@ pub fn solution_part_1() -> () {
     let content = include_str!("input/day_04_part_1_test_input.txt");
     // let content = include_str!("input/day_04_input.txt");
 
-    let input_chunks = Regex::new(r"Card (\d+): (.*) \| (.*)").unwrap();
+    let input_chunks = Regex::new(r"Card (\d+): +(.*) \| +(.*)").unwrap();
 
+    let mut card_scores: Vec<u32> = Vec::new();
     content.lines().for_each(|line| {
         // Provide a mechanism to limit the volume of output on the console.
         iteration += 1;
@@ -37,24 +38,37 @@ pub fn solution_part_1() -> () {
 
         if let Some(caps) = input_chunks.captures(line) {
             let card = caps.get(1).map_or("", |m| m.as_str());
-            postMessageToWorker(show_message, &format!("card: {}", card));
             let card_int = card.parse::<u32>().expect("Should be able to parse game");
             let winning_numbers_as_string = caps.get(2).map_or("", |m| m.as_str());
             let our_numbers_as_string = caps.get(3).map_or("", |m| m.as_str());
             let winning_numbers = split_digits_over_whitespace(winning_numbers_as_string);
             let our_numbers = split_digits_over_whitespace(our_numbers_as_string);
+            let intersection: Vec<_> = winning_numbers.iter().filter(|&n| our_numbers.contains(n)).collect();
+            let intersection_count = intersection.len() as u32;
+            let mut card_score = 0;
+            if intersection_count > 0 {
+                card_score = 2_u32.pow(intersection_count - 1);
+            }
+            card_scores.push(card_score);
+            postMessageToWorker(show_message, &format!("card: {}", card));
             postMessageToWorker(show_message, &format!("winning numbers: {:?}", winning_numbers));
             postMessageToWorker(show_message, &format!("our numbers: {:?}", our_numbers));
+            postMessageToWorker(show_message, &format!("our winners: {:?}", intersection));
+            postMessageToWorker(show_message, &format!("card score: {:?}", card_score));
         }
 
     });
+    let sum: u32 = card_scores.iter().sum();
+    postMessageToWorker(true, &format!("⭐️ Total cards score: {}", sum));
 }
 
 fn split_digits_over_whitespace(input: &str) -> Vec<u32> {
+    console::log_1(&format!("input: {}", input).into());
     let whitespace = Regex::new(r"\s+").unwrap();
     let parts_as_strings: Vec<&str> = whitespace.split(input).collect();
     let mut found_numbers: Vec<u32> = Vec::new();
     for number in parts_as_strings {
+        console::log_1(&format!("number: {}", number).into());
         let number_as_int = number.parse::<u32>().expect("Should be able to parse number");
         found_numbers.push(number_as_int);
     }
