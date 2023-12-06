309bc0ad79f421f0aaff38a7c7e8cf6d1482c57c
Frank Hereford <frank@frankhereford.com>
2023-12-06 05:27:46 +0000

    Refactor and cleanup day_05.rs almanac parsing
    Refactored the parse_alamanac function to use a clearer, more concise loop structure and eliminated commented-out code blocks. Changed the type of keys in the HashMap from &str to String for better data management and extensibility. Removed unnecessary cloning and verbose output to streamline the parser's operation. These changes improve readability, maintainability, and performance of the almanac parsing logic.

--- rust/src/solutions/day_05.rs
+++ rust/src/solutions/day_05.rs
@@ -16,30 +16,12 @@ pub fn solution_part_1() -> () {
     // let content = include_str!("input/day_05_input.txt");
 
     let almanac = parse_alamanac(content);
+    postMessageToWorker(true, &format!("Almanac: {:?}", almanac));
 
-    /*
-    content.lines().for_each(|line| {
-        // Provide a mechanism to limit the volume of output on the console.
-        iteration += 1;
-        let mut show_message = false;
-        if (iteration) % 300 == 0  {
-            show_message = true;
-        }
-
-        let characters: Vec<_> = line.chars().collect();
-        if characters[0] == '#' {
-            // postMessageToWorker(show_message, "Skipping line because it is a comment.");
-            return;
-        }
-
-        postMessageToWorker(show_message, " ");
-        postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
-    });
-    */
 }
 
 
-fn parse_alamanac(content: &str) -> HashMap<&str, HashMap<u32, u32>> {
+fn parse_alamanac(content: &str) -> HashMap<String, HashMap<u32, u32>> {
     let show_message = true;
 
     let line_count = content.lines().count();
@@ -49,7 +31,8 @@ fn parse_alamanac(content: &str) -> HashMap<&str, HashMap<u32, u32>> {
     let map_detector = Regex::new(r"(\w+)-to-(\w+) map").unwrap();
     let digit_detector = Regex::new(r"\d").unwrap();
 
-    //for line_number in 0..line_count {
+    let mut almanac: HashMap<String, HashMap<u32, u32>> = HashMap::new();
+
     while line_number < line_count {
         let line = content.lines().nth(line_number).unwrap();
         postMessageToWorker(show_message, &format!("Parsing line: {}", line));
@@ -64,18 +47,29 @@ fn parse_alamanac(content: &str) -> HashMap<&str, HashMap<u32, u32>> {
             let destination = captures.get(2).map_or("", |m| m.as_str());
             postMessageToWorker(show_message, &format!("Origin: {}, Destination: {}", origin, destination));
 
+            let key = format!("{}-{}", origin, destination);
+            almanac.insert(key.clone(), HashMap::new()); // Clone `key` here
+
             line_number += 1;
             loop {
                 if line_number >= line_count {
                     break;
                 }
                 let line = content.lines().nth(line_number).unwrap();
-                postMessageToWorker(show_message, &format!("Parsing line!!: {}", line));
+                // postMessageToWorker(show_message, &format!("Parsing line!!: {}", line));
                 if digit_detector.is_match(&line) {
                     line_number += 1;
-                    postMessageToWorker(show_message, &format!("line: {}", line));
+                    // postMessageToWorker(show_message, &format!("line: {}", line));
                     let digits = split_digits_over_whitespace(line);
                     postMessageToWorker(show_message, &format!("digits: {:?}", digits));
+                    if let Some(entry) = almanac.get_mut(&key) {
+                        for i in 0..digits[2] {
+                            let range_key = digits[1] + i;
+                            let range_value = digits[0] + i;
+                            postMessageToWorker(show_message, &format!("range_key: {}, range_value: {}", range_key, range_value));
+                            entry.insert(range_key, range_value);
+                        }
+                    }
                 } else {
                     break;
                 }
@@ -86,7 +80,6 @@ fn parse_alamanac(content: &str) -> HashMap<&str, HashMap<u32, u32>> {
         line_number += 1; 
     }
 
-    let almanac: HashMap<&str, HashMap<u32, u32>> = HashMap::new();
     almanac
 }
 
