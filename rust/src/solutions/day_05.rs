ef4d4d26b42d3036395d4b32d84c3b70b488c745
Frank Hereford <frank@frankhereford.com>
2023-12-06 04:44:31 +0000

    Enhance day_05 with parsing functions and regex
    Introduced regular expression matching and a new `parse_almanac` function to extract structured data from input in `day_05.rs`. Implemented additional utilities for processing text input, facilitating future extensions and features. These changes do not yet impact existing functionality but lay the groundwork for more sophisticated input handling.

--- rust/src/solutions/day_05.rs
+++ rust/src/solutions/day_05.rs
@@ -2,6 +2,8 @@
 #![allow(unused_variables)]
 //use web_sys::console;
 use wasm_bindgen::prelude::*;
+use std::collections::HashMap;
+use regex::Regex;
 
 #[wasm_bindgen(module = "/src/solutions/workerHelpers.js")]
 extern "C" {
@@ -10,10 +12,12 @@ extern "C" {
 
 pub fn solution_part_1() -> () {
     postMessageToWorker(true, "Part 1: \n");
-    let mut iteration = -1;
     let content = include_str!("input/day_05_part_1_test_input.txt");
     // let content = include_str!("input/day_05_input.txt");
 
+    let almanac = parse_alamanac(content);
+
+    /*
     content.lines().for_each(|line| {
         // Provide a mechanism to limit the volume of output on the console.
         iteration += 1;
@@ -31,8 +35,54 @@ pub fn solution_part_1() -> () {
         postMessageToWorker(show_message, " ");
         postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
     });
+    */
 }
 
+
+fn parse_alamanac(content: &str) -> HashMap<&str, HashMap<u32, u32>> {
+    let show_message = true;
+
+    let line_count = content.lines().count();
+    let mut line_number = 0;
+
+    let seed_detector = Regex::new(r"seeds: ([\d ]+)").unwrap();
+    let map_detector = Regex::new(r"(\w+)-to-(\w+) map").unwrap();
+
+    //for line_number in 0..line_count {
+    while line_number < line_count {
+        let line = content.lines().nth(line_number).unwrap();
+
+        if let Some(captures) = seed_detector.captures(line) {
+            let seeds = split_digits_over_whitespace(captures.get(1).map_or("", |m| m.as_str()));
+            postMessageToWorker(show_message, &format!("Seeds: {:?}", seeds));
+        }
+
+        if let Some(captures) = map_detector.captures(line) {
+            let origin = captures.get(1).map_or("", |m| m.as_str());
+            let destination = captures.get(2).map_or("", |m| m.as_str());
+            postMessageToWorker(show_message, &format!("Origin: {}, Destination: {}", origin, destination))
+        }
+
+        postMessageToWorker(show_message, &format!("Parsing line: {}", line));
+        line_number += 1; 
+    }
+
+    let almanac: HashMap<&str, HashMap<u32, u32>> = HashMap::new();
+    almanac
+}
+
+fn split_digits_over_whitespace(input: &str) -> Vec<u32> {
+    //console::log_1(&format!("input: {}", input).into());
+    let whitespace = Regex::new(r"\s+").unwrap();
+    let parts_as_strings: Vec<&str> = whitespace.split(input).collect();
+    let mut found_numbers: Vec<u32> = Vec::new();
+    for number in parts_as_strings {
+        //console::log_1(&format!("number: {}", number).into());
+        let number_as_int = number.parse::<u32>().expect("Should be able to parse number");
+        found_numbers.push(number_as_int);
+    }
+    found_numbers
+}
 
 pub fn solution_part_2() -> () {
     return;
