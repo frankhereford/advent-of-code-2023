10fa91cd68c06f521f97f65abb07ec73cbd1cde0
Frank Hereford <frank@frankhereford.com>
2023-12-11 13:18:17 +0000

    Add card and hand types

--- rust/src/solutions/day_07.rs
+++ rust/src/solutions/day_07.rs
@@ -4,11 +4,20 @@
 use wasm_bindgen::prelude::*;
 
 use regex::Regex;
+use radix_fmt::radix;
+
 
 #[wasm_bindgen(module = "/src/solutions/workerHelpers.js")]
 extern "C" {
     fn postMessageToWorker(do_print: bool, message: &str);
 }
+
+type Card = char;
+type Hand = [Card; 5];
+type Wager = u32;
+type ScoringClassification = (String, String);
+type CardHand = (Hand, Wager, ScoringClassification);
+type CardHands = Vec<CardHand>;
 
 pub fn solution_part_1() -> () {
     postMessageToWorker(true, "Part 1: \n");
@@ -16,13 +25,13 @@ pub fn solution_part_1() -> () {
     let content = include_str!("input/day_07_test_input.txt");
     // let content = include_str!("input/day_07_input.txt");
 
-    let hand = Regex::new(r"(\w{5}) (\d+)").unwrap();
+    let hand_regex = Regex::new(r"(\w{5}) (\d+)").unwrap();
 
     content.lines().for_each(|line| {
         // Provide a mechanism to limit the volume of output on the console.
         iteration += 1;
         let mut show_message = false;
-        if (iteration) % 300 == 0  {
+        if (iteration) % 1 == 0  {
             show_message = true;
         }
 
@@ -34,6 +43,12 @@ pub fn solution_part_1() -> () {
 
         postMessageToWorker(show_message, " ");
         postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
+
+        if let Some(caps) = hand_regex.captures(line) {
+            let cards = caps.get(1).map_or("", |m| m.as_str().trim());
+            let wager = caps.get(2).map_or("", |m| m.as_str().trim());
+        }
+
     });
 }
 
