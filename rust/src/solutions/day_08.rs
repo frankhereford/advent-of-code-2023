1e190cc7e73c77a293e1e975e236c1be5d58cb86
Frank Hereford <frank@frankhereford.com>
2023-12-12 12:03:33 +0000

    Add solution framework for Day 08 Part 2

--- rust/src/solutions/day_08.rs
+++ rust/src/solutions/day_08.rs
@@ -55,4 +55,28 @@ pub fn solution_part_1() -> () {
 
 
 pub fn solution_part_2() -> () {
+    postMessageToWorker(true, "Part 2: \n");
+    let content = include_str!("input/day_08_part_2_test_input.txt");
+    //let content = include_str!("input/day_08_part_1_input.txt");
+    let input = content.lines().collect::<Vec<&str>>();
+    let instructions = input[0].chars().collect::<Vec<char>>();
+    let mut instruction = instructions.iter().cycle(); // can just access this forever
+    let linkage_regex = Regex::new(r"(\w{3}) = \((\w{3}), (\w{3})\)").unwrap();
+    let mut linkages: HashMap<String, HashMap<String, String>> = HashMap::new();
+    let mut starting_locations: Vec<String> = Vec::new();
+    for i in 2..input.len() {
+        let linkage = input[i];
+        let captures = linkage_regex.captures(linkage).unwrap();
+        let location = captures.get(1).unwrap().as_str();
+        let left = captures.get(2).unwrap().as_str();
+        let right = captures.get(3).unwrap().as_str();
+        let mut destionations: HashMap<String, String> = HashMap::new();
+        destionations.insert("left".to_string(), left.to_string());
+        destionations.insert("right".to_string(), right.to_string());
+        linkages.insert(location.to_string(), destionations);
+        if location.chars().nth(2).unwrap() == 'A' {
+            starting_locations.insert(0, location.to_string());
+        }
+    }
+    postMessageToWorker(true, &format!("starting_locations: {:?}", starting_locations));
 }
