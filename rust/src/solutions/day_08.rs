8c7e02ce2db8e4a94f0d54be05c97834fa75bc33
Frank Hereford <frank@frankhereford.com>
2023-12-12 11:29:07 +0000

    create cycle iterator

--- rust/src/solutions/day_08.rs
+++ rust/src/solutions/day_08.rs
@@ -9,21 +9,11 @@ extern "C" {
 
 pub fn solution_part_1() -> () {
     postMessageToWorker(true, "Part 1: \n");
-    let mut iteration = -1;
     let content = include_str!("input/day_08_part_1_test_1_input.txt");
     // let content = include_str!("input/day_08_input.txt");
-
-    content.lines().for_each(|line| {
-        // Provide a mechanism to limit the volume of output on the console.
-        iteration += 1;
-        let mut show_message = false;
-        if (iteration) % 1 == 0  {
-            show_message = true;
-        }
-
-        postMessageToWorker(show_message, " ");
-        postMessageToWorker(show_message, &format!("Iteration: {}, input: {}", iteration, line));
-    });
+    let input = content.lines().collect::<Vec<&str>>();
+    let instructions = input[0].chars().collect::<Vec<char>>();
+    let mut instruction = instructions.iter().cycle(); // can just access this forever
 }
 
 
