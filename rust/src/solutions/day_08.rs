30620a00fd3b79d512c0c7d68dc90e912336d1ea
Frank Hereford <frank@frankhereford.com>
2023-12-12 13:31:25 +0000

    part 2!

--- rust/src/solutions/day_08.rs
+++ rust/src/solutions/day_08.rs
@@ -3,7 +3,7 @@
 //use web_sys::console;
 use wasm_bindgen::prelude::*;
 use regex::Regex;
-use std::collections::{HashMap};
+use std::collections::{HashSet, HashMap};
 
 #[wasm_bindgen(module = "/src/solutions/workerHelpers.js")]
 extern "C" {
@@ -11,7 +11,6 @@ extern "C" {
 }
 
 pub fn solution_part_1() -> () {
-    return;
     postMessageToWorker(true, "Part 1: \n");
     //let content = include_str!("input/day_08_part_1_test_1_input.txt");
     let content = include_str!("input/day_08_part_1_input.txt");
@@ -41,7 +40,7 @@ pub fn solution_part_1() -> () {
             &'R' => linkages.get(current_location).unwrap().get("right").unwrap(),
             _ => linkages.get(current_location).unwrap().get("left").unwrap(),
         };
-        if move_count % 1000 == 0 {
+        if move_count % 2000 == 0 {
             postMessageToWorker(true, &format!("move {} - from: {}, via: {}, to: {}", move_count, current_location, instruction, next_location));
         }
         if next_location == end {
@@ -60,7 +59,6 @@ pub fn solution_part_2() -> () {
     let content = include_str!("input/day_08_part_1_input.txt");
     let input = content.lines().collect::<Vec<&str>>();
     let instructions = input[0].chars().collect::<Vec<char>>();
-    let mut instruction = instructions.iter().cycle();
     let linkage_regex = Regex::new(r"(\w{3}) = \((\w{3}), (\w{3})\)").unwrap();
     let mut linkages: HashMap<String, HashMap<String, String>> = HashMap::new();
     let mut paths: Vec<String> = Vec::new();
@@ -79,33 +77,51 @@ pub fn solution_part_2() -> () {
         }
     }
     postMessageToWorker(true, &format!("paths: {:?}", paths));
-    let mut move_count = 0;
-    loop {
-        move_count += 1;
-        if move_count % 100000 == 0 {
-            postMessageToWorker(true, &format!("move: {}", move_count));
-        }
-        let instruction = instruction.next().unwrap();
-        for i in 0..paths.len() {
-            let current_location = &paths[i];
+    let mut path_distances: Vec<u64> = Vec::new();
+    for i in 0..paths.len() {
+        postMessageToWorker(true, &format!("paths[{}]: {:?}", i, paths[i]));
+        let mut move_count = 0;
+        let mut instruction = instructions.iter().cycle();
+        let mut current_location = &paths[i];
+        loop {
+            move_count += 1;
+            let instruction = instruction.next().unwrap();
             let next_location = match instruction {
                 &'R' => linkages.get(current_location).unwrap().get("right").unwrap(),
                 _ => linkages.get(current_location).unwrap().get("left").unwrap(),
             };
-            if move_count % 10000 == 0 {
-                //postMessageToWorker(true, &format!("move {} - from: {}, via: {}, to: {}", move_count, current_location, instruction, next_location));
-            }
-            paths[i] = next_location.to_string();
-        }
-        let mut end_location_count = 0;
-        for i in 0..paths.len() {
-            if paths[i].chars().nth(2).unwrap() == 'Z' {
-                end_location_count += 1;
-            }
-        }
-        if end_location_count == paths.len() {
-            postMessageToWorker(true, &format!("Found the end on move: {}", move_count));
+            if next_location.chars().nth(2).unwrap() == 'Z' {
+                postMessageToWorker(true, &format!("found end on move {} - from: {}, via: {}, to: {}", move_count, current_location, instruction, next_location));
+                path_distances.push(move_count);
             break;
+            }
+            current_location = next_location;
         }
     }
+    postMessageToWorker(true, &format!("path_distances: {:?}", path_distances));
+
+    let mut common_factors: HashSet<u64> = HashSet::new();
+    for distance in path_distances {
+        let factors = factorize(distance);
+        for factor in factors {
+            common_factors.insert(factor);
+        }
+    }
+    let product: u64 = common_factors.iter().product();
+    postMessageToWorker(true, &format!("common_factors: {:?}, product: {}", common_factors, product));
+}
+
+fn factorize(mut n: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    let mut divisor = 2;
+
+    while n > 1 {
+        while n % divisor == 0 {
+            factors.push(divisor);
+            n /= divisor;
+        }
+        divisor += 1;
+    }
+
+    factors
 }
