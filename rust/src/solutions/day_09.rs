dcf4fe8c3a2676a3fe714422b5767c217d6ae378
Frank Hereford <frank@frankhereford.com>
2023-12-12 21:22:19 +0000

    Add day 09 solution and update maxDay in package.json

---
+++ rust/src/solutions/day_09.rs
@@ -0,0 +1,26 @@
+#![allow(unused_variables)]
+#![allow(unreachable_code)]
+//use web_sys::console;
+use wasm_bindgen::prelude::*;
+//use regex::Regex;
+//use std::collections::{HashSet, HashMap};
+
+#[wasm_bindgen(module = "/src/solutions/workerHelpers.js")]
+extern "C" {
+    fn postMessageToWorker(do_print: bool, message: &str);
+}
+
+pub fn solution_part_1() -> () {
+    postMessageToWorker(true, "Part 1: \n");
+    let content = include_str!("input/day_09_part_1_test_input.txt");
+    //let content = include_str!("input/day_09_part_1_input.txt");
+
+}
+
+
+pub fn solution_part_2() -> () {
+    return;
+    postMessageToWorker(true, "Part 2: \n");
+    let content = include_str!("input/day_09_part_1_test_input.txt");
+    //let content = include_str!("input/day_09_part_1_input.txt");
+}
