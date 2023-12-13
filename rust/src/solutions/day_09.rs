99ef3089ae18fff0caf476d6e774c5853e8fb9ea
Frank Hereford <frank@frankhereford.com>
2023-12-13 16:06:17 +0000

    here's part 1

--- rust/src/solutions/day_09.rs
+++ rust/src/solutions/day_09.rs
@@ -11,12 +11,14 @@ extern "C" {
 
 pub fn solution_part_1() -> () {
     postMessageToWorker(true, "Part 1: \n");
-    let content = include_str!("input/day_09_part_1_test_input.txt");
-    //let content = include_str!("input/day_09_part_1_input.txt");
+    //let content = include_str!("input/day_09_part_1_test_input.txt");
+    let content = include_str!("input/day_09_part_1_input.txt");
 
     let mut iteration = -1;
+    let mut added_values: Vec<i32> = Vec::new();
     content.lines().for_each(|line| {
         iteration += 1;
+        postMessageToWorker(true, &format!(""));
         let mut digits: Vec<i32> = line.split_whitespace()
                         .map(|s| s.parse::<i32>().unwrap())
                         .collect();
@@ -25,13 +27,41 @@ pub fn solution_part_1() -> () {
         pyramid.push(digits.clone());
         loop {
             digits = calculate_differences(&digits);
-            postMessageToWorker(true, &format!("{}: {:?}", iteration, digits));
+            pyramid.push(digits.clone());
+            //postMessageToWorker(true, &format!("{}: {:?}", iteration, digits));
             let is_done = check_all_zeros(&digits);
             if is_done {
                 break;
             }
         }
+
+        //postMessageToWorker(true, &format!("{}: {:?}", iteration, pyramid));
+
+        let mut previous_rightmost_value = 0;
+        for i in 0..pyramid.len() {
+            let n = pyramid.len() - i - 1;
+            //postMessageToWorker(true, &format!("{}/ i: {}, n: {}", iteration, i, n));
+            if n == pyramid.len() - 1 {
+                let row_length = pyramid[n].len();
+                pyramid[n].insert(row_length, 0);
+            }
+            else {
+                let rightmost_value_location = pyramid[n].len() - 1; 
+                let rightmost_value = pyramid[n].get(rightmost_value_location).unwrap();
+                let new_value = rightmost_value + previous_rightmost_value;
+                pyramid[n].insert(rightmost_value_location + 1, new_value);
+                previous_rightmost_value = new_value;
+                if n == 0 {
+                    added_values.push(new_value);
+                }
+            }
+            //postMessageToWorker(true, &format!("{}: {:?}", iteration, pyramid[n]));
+        }
     });
+
+    let sum: i32 = added_values.iter().sum();
+    postMessageToWorker(true, &format!("{}/ {:?}", iteration, added_values));
+    postMessageToWorker(true, &format!("{}/ {:?}", iteration, sum));
 
 }
 
