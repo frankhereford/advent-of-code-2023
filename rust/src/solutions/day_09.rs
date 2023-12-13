0c1c19ad5551fd62d31335532671c4dc73529290
Frank Hereford <frank@frankhereford.com>
2023-12-13 01:18:43 +0000

    Refactor calculation of differences and add check for all zeros

--- rust/src/solutions/day_09.rs
+++ rust/src/solutions/day_09.rs
@@ -11,14 +11,48 @@ extern "C" {
 
 pub fn solution_part_1() -> () {
     postMessageToWorker(true, "Part 1: \n");
-    //let content = include_str!("input/day_09_part_1_test_input.txt");
-    let content = include_str!("input/day_09_part_1_input.txt");
+    let content = include_str!("input/day_09_part_1_test_input.txt");
+    //let content = include_str!("input/day_09_part_1_input.txt");
 
     let mut iteration = -1;
     content.lines().for_each(|line| {
         iteration += 1;
+        let mut digits: Vec<i32> = line.split_whitespace()
+                        .map(|s| s.parse::<i32>().unwrap())
+                        .collect();
+
+        let mut pyramid: Vec<Vec<i32>> = Vec::new();
+        pyramid.push(digits.clone());
+        loop {
+            digits = calculate_differences(&digits);
+            postMessageToWorker(true, &format!("{}: {:?}", iteration, digits));
+            let is_done = check_all_zeros(&digits);
+            if is_done {
+                break;
+            }
+        }
     });
 
+}
+
+fn calculate_differences(digits: &[i32]) -> Vec<i32> {
+    let mut differences: Vec<i32> = Vec::new();
+    for i in 0..digits.len() - 1 {
+        let difference: i32 = digits[i+1] - digits[i];
+        differences.push(difference);
+    }
+    differences
+}
+
+fn check_all_zeros(digits: &[i32]) -> bool {
+    let mut all_zeros = true;
+    for i in 0..digits.len() {
+        if digits[i] != 0 {
+            all_zeros = false;
+            break;
+        }
+    }
+    all_zeros
 }
 
 pub fn solution_part_2() -> () {
